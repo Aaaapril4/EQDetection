a5c55d32dd07b68495c371a32753c075ad063581
Aaaapril4 <gia.jyq@gmail.com>
2024-07-30 15:56:23 -0400

    script to create environment

---
+++ env_icer.sh
@@ -0,0 +1,22 @@
+# check python environment first
+# clean all environment
+module purge
+deactivate
+conda deactivate
+
+# load python module
+module load GCCcore/12.3.0
+module load Python/3.11.3
+
+# create virtual env
+python3 -m venv ~/phasenet-tf
+source ~/phasenet-tf/bin/activate
+conda create -n pntf python=3.11.3
+conda activate pntf
+
+git clone https://github.com:Aaaapril4/PhaseNet-TF.git
+conda install conda-forge::poetry
+
+cd PhaseNet-TF
+rm *lock
+poetry install --with train
